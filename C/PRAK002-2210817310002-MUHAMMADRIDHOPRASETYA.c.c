36d5ee9a4ad93a54a60c25bb1c103bdce837494c
pttmcdo <109886471+pttmcdo@users.noreply.github.com>
2022-10-09 00:16:33 +0800

    Add files via upload

---
+++ C/PRAK002-2210817310002-MUHAMMADRIDHOPRASETYA.c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Selamat pagi, Muhammad Ridho Prasetya\n");
+    printf("Selamat siang, Muhammad Ridho Prasetya\n");
+    printf("Selamat malam, Muhammad Ridho Prasetya\n");
+}
---
+++ PYTHON/PRAK002-2210817310002-MUHAMMADRIDHOPRASETYA.py.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Muhammad Ridho Prasetya")
+print("Selamat Siang, Muhammad Ridho Prasetya")
+print("Selamat Malam, Muhammad Ridho Prasetya")
