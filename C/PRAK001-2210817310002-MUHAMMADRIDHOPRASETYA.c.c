17cd7f71cc4950ebbfe45e015136c7c9506576ae
pttmcdo <109886471+pttmcdo@users.noreply.github.com>
2022-10-16 22:08:38 +0800

    Delete C directory

--- C/PRAK001-2210817310002-MUHAMMADRIDHOPRASETYA.c.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Saya programmer No.1");
-    return 0;
-}
--- C/PRAK002-2210817310002-MUHAMMADRIDHOPRASETYA.c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Selamat pagi, Muhammad Ridho Prasetya\n");
-    printf("Selamat siang, Muhammad Ridho Prasetya\n");
-    printf("Selamat malam, Muhammad Ridho Prasetya\n");
-}
