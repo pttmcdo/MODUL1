931c3b673d127a84a38b23323f11ff5afc425b88
pttmcdo <109886471+pttmcdo@users.noreply.github.com>
2022-10-16 22:08:48 +0800

    Delete PYTHON directory

--- PYTHON/PRAK002-2210817310002-MUHAMMADRIDHOPRASETYA.py.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Muhammad Ridho Prasetya")
-print("Selamat Siang, Muhammad Ridho Prasetya")
-print("Selamat Malam, Muhammad Ridho Prasetya")
--- PYTHON/PRAK003-2210817310002-MUHAMMADRIDHOPRASETYA.c.py
+++
@@ -1,1 +0,0 @@
-print("Andi berkata \"Saya Pasti Bisa\"")
