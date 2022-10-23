43117a0569d73901e2d4c5f674299f3d3e7cb0ff
pttmcdo <109886471+pttmcdo@users.noreply.github.com>
2022-10-23 21:10:27 +0800

    Delete MODUL 3 directory

--- MODUL 3/Soal 2/PRAK202-2210817310002-MUHAMMAD RIDHO PRASETYA.py.py
+++
@@ -1,6 +0,0 @@
-a = input('Masukkan Nilai Pertama: ') 
-b = input('Masukkan Nilai Kedua: ') 
- 
-hasil = float(a) + float(b) 
-
-print('Hasil dari penjumlahan nilai pertama {0} dan nilai kedua {1} adalah {2}'.format(a, b, hasil))
--- MODUL 3/Soal 3/PRAK203-2210817310002-MUHAMMAD RIDHO PRASETYA.py.py
+++
@@ -1,9 +0,0 @@
-a = input('')
-b = input('')
-i = input('')
-j = input('')
-x = input('')
-y = input('')
-hasil = (float(a)-float(b))*(float(i)/float(j))-(float(x)+float(y))
-print(round(hasil,3))
-
